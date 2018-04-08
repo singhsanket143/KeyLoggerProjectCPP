7ef67950610ab85f096b90efe7798b8343aa414c
singhsanket143 <singhsanket143@gmail.com>
2018-04-08 13:38:46 +0530

    Initial Commit

---
+++ Helper.h
@@ -0,0 +1,72 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <ctime>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+namespace Helper
+{
+template <class T>
+std::string ToString(const T &);
+
+struct DateTime
+{
+    DateTime()
+    {
+        time_t ms;
+        time(&ms);
+
+        struct tm *info = localtime(&ms);
+        D = info->tm_mday;
+        m = info->tm_mon + 1;   // because it starts from 0 e.g. January = 0
+        Y = 1900 + info->tm_year;
+        M = info->tm_min;
+        H = info->tm_hour;
+        S = info->tm_sec;
+    }
+
+    DateTime(int D, int m, int Y, int H, int M, int S) : D(D), m(m), Y(Y), H(H), M(M), S(S) {}
+    DateTime(int D, int m, int Y) : D(D), m(m), Y(Y), H(0), M(0), S(0) {}
+
+    DateTime Now() const
+    {
+        return DateTime();
+    }
+
+    int D, m, Y, H, M, S;
+
+    std::string GetDateString() const
+    {
+        return std::string(D < 10 ? "0": "") + ToString(D) + std::string(m < 10? ".0": ".") + ToString(m) + ToString(Y);
+    }
+
+    std::string GetTimeString(const std::string &sep = ":")
+    {
+        return std::string(H < 10? "0": "") + ToString(H) + sep + std::string(M < 10? "0": "") + ToString(M) + sep + std::string(S < 10? sep: "") + ToString(S);
+    }
+
+    std::string GetDateTimeString(const std::string &sep = ":")
+    {
+        return GetDateString() + " " + GetTimeString(sep);
+    }
+};
+
+    template <class T>
+    std::string ToString(const T &e)
+    {
+        std::ostringstream s;
+        s << e;
+        return s.str();
+    }
+
+    void WriteAppLog (const std::string &s)
+    {
+        std::ofstream file("AppLog.txt", std::ios::app);
+        file << "[" << Helper::DateTime().GetDateTimeString() << "]" << "\n" << s << std::endl << "\n";
+        file.close();
+    }
+}
+#endif // HELPER_H
+
